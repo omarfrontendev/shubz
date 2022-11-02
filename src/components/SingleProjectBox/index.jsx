13fa6b6dcdff74f28cedea9f8317deb587c77467
omarfrontendev <omar.abdelrhman.dev@gmail.com>
2022-11-02 05:24:47 +0200

    musics

--- src/components/SingleProjectBox/index.jsx
+++ src/components/SingleProjectBox/index.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 
 import './SingleProjectBox.css'
 
-const SingleProjectBox = ({ name, singer, img, audio }) => {
+const SingleProjectBox = ({ name, img, audio }) => {
   return (
     <div className='single__project__box'>
       <div className='project__img__container'>
         <img className='project__img' src={img || ''} alt="" />
       </div>
       <span className='project__name'>{name}</span>
-      <audio src={audio} type='audio'  controls/>
+      <audio controls>
+        <source src={'http://docs.google.com/uc?export=open&amp;id=1uZd99P8_xWM3fYsoSl4xHa8md2MWHa68'} type='audio/mp3' />
+      </audio>
       {/* <p className='project__signer'>{singer}</p> */}
     </div>
   )
 }
 
-export default SingleProjectBox
+export default SingleProjectBox;
